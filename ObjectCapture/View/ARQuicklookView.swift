c2ab857b90ecfaeeef97b248fc32dfdc02fca0ff
koreadeep32 <koreadeep32@gmail.com>
2023-06-19 18:41:11 +0900

    Add reference link to ARQuickLook for SwiftUI

--- ObjectCapture/View/ARQuicklookView.swift
+++ ObjectCapture/View/ARQuicklookView.swift
@@ -4,36 +4,29 @@
 //
 //  Created by koreadeep32 on 2023/06/16.
 //
+// ARQuickLook source, help from: 
+// https://developer.apple.com/forums/thread/126377
 
 import SwiftUI
 import QuickLook
 import ARKit
 
 struct ARQuickLookView: UIViewControllerRepresentable {
-    // Properties: the file name (without extension), and whether we'll let
-    // the user scale the preview content.
     var name: String
     var allowScaling: Bool = true
     var captureDir: URL
     
     func makeCoordinator() -> ARQuickLookView.Coordinator {
-        // The coordinator object implements the mechanics of dealing with
-        // the live UIKit view controller.
         Coordinator(self)
     }
     
     func makeUIViewController(context: Context) -> QLPreviewController {
-        // Create the preview controller, and assign our Coordinator class
-        // as its data source.
         let controller = QLPreviewController()
         controller.dataSource = context.coordinator
         return controller
     }
     
-    func updateUIViewController(_ controller: QLPreviewController,
-                                context: Context) {
-        // nothing to do here
-    }
+    func updateUIViewController(_ controller: QLPreviewController, context: Context) { }
     
     class Coordinator: NSObject, QLPreviewControllerDataSource {
         let parent: ARQuickLookView
@@ -45,13 +38,10 @@ struct ARQuickLookView: UIViewControllerRepresentable {
             super.init()
         }
         
-        // The QLPreviewController asks its delegate how many items it has:
         func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
             return 1
         }
         
-        // For each item (see method above), the QLPreviewController asks for
-        // a QLPreviewItem instance describing that item:
         func previewController(
             _ controller: QLPreviewController,
             previewItemAt index: Int
